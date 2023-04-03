32940d7dce9e33bd15102159ec65aadac42a99fb
Russell Fustino <rfustino@hotmail.com>
2023-04-03 13:13:46 -0400

    Replaced testnet dispenser code with kmd account funding for the created standalone account

--- python/accounts/create_account.py
+++ python/accounts/create_account.py
@@ -1,83 +1,70 @@
 import base64
+from algosdk.future import transaction
 import json
-
-from algosdk import account, mnemonic, transaction
+from algosdk import account
+from algosdk import mnemonic
 from algosdk.v2client import algod
 
+from sandbox import get_accounts
+
+
 # # This is a complete code example that:
-# #   1. Creates a new test account
-# #   2. Ask to fund your created account
+# #   1. Create a new account
+# #   2. Fund your created account from another one
+
 
 def create_account():
     # Change algod_token and algod_address to connect to a different client
-	algod_address = "http://localhost:4001"
-	algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-	algod_client = algod.AlgodClient(algod_token, algod_address)
+    algod_address = "http://localhost:4001"
+    algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
+    algod_client = algod.AlgodClient(algod_token, algod_address)
+    sendingaddr, sendingsk = get_accounts()[0]
     # Generate new account for this transaction
-	secret_key, my_address = account.generate_account()
-    
-	print("My address: {}".format(my_address))
-	print("My private key: {}".format(secret_key))
-	print("My mnemonic: {}".format(mnemonic.from_private_key(secret_key)))
+    my_secret_key, my_address = account.generate_account()
+
+    print("My address: {}".format(my_address))
+    print("My private key: {}".format(my_secret_key))
+    print("My mnemonic: {}".format(mnemonic.from_private_key(my_secret_key)))
     # Check your balance. It should be 0 microAlgos
 
+    account_info = algod_client.account_info(my_address)
+    print("Account balance: {} microAlgos".format(
+        account_info.get('amount')) + "\n")
 
-	account_info = algod_client.account_info(my_address)
-	print("Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
-
-    # Fund the created account
-	print('Fund the created account using testnet faucet: \n https://dispenser.testnet.aws.algodev.network/?account=' + format(my_address))
-
-	completed = ""
-
-	while completed.lower() != 'yes':
-		completed = input("Type 'yes' once you funded the account: ");
-    
-	print('Fund transferred!')
-    # Check your balance. It should be 5000000 microAlgos
-	account_info = algod_client.account_info(my_address)
-	print("Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
     # build transaction
-	print("Building transaction")
-	params = algod_client.suggested_params()
-    # comment out the next two (2) lines to use suggested fees
-	# params.flat_fee = True
-	# params.fee = 1000
-	receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA"
-	note = "Hello World".encode()
-	amount = 100000
-	closeto= "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA"
-    # Fifth argument is a close_remainder_to parameter that creates a payment txn that sends all of the remaining funds to the specified address. If you want to learn more, go to: https://developer.algorand.org/docs/reference/transactions/#payment-transaction
-	unsigned_txn = transaction.PaymentTxn(my_address, params, receiver, amount, closeto, note)
+    print("Building transaction")
+    params = algod_client.suggested_params()
+
+    amount = 100000
+    unsigned_txn = transaction.PaymentTxn(
+        sendingaddr, params, my_address, amount)
 
     # sign transaction
-	print("Signing transaction")
-	signed_txn = unsigned_txn.sign(secret_key)
-	print("Sending transaction")
-	txid = algod_client.send_transaction(signed_txn)
-	print('Transaction Info:')
-	print("Signed transaction with txID: {}".format(txid))
+    print("Signing transaction")
+    signed_txn = unsigned_txn.sign(sendingsk)
+    print("Sending transaction")
+    txid = algod_client.send_transaction(signed_txn)
+    print('Transaction Info:')
+    print("Signed transaction with txID: {}".format(txid))
 
-    # wait for confirmation	
-	try:
-		print("Waiting for confirmation")
-		confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)  
-	except Exception as err:
-		print(err)
-		return
-	print("txID: {}".format(txid), " confirmed in round: {}".format(confirmed_txn.get("confirmed-round", 0))) 
-	print("Transaction information: {}".format(
+    # wait for confirmation
+    try:
+        print("Waiting for confirmation")
+        confirmed_txn = transaction.wait_for_confirmation(
+            algod_client, txid, 4)
+    except Exception as err:
+        print(err)
+        return
+    print("txID: {}".format(txid), " confirmed in round: {}".format(
+        confirmed_txn.get("confirmed-round", 0)))
+    print("Transaction information: {}".format(
         json.dumps(confirmed_txn, indent=4)))
-	print("Decoded note: {}".format(base64.b64decode(
-        confirmed_txn["txn"]["txn"]["note"]).decode()))
-	print("Starting Account balance: {} microAlgos".format(account_info.get('amount')) )
-	print("Amount transfered: {} microAlgos".format(amount) )    
-	print("Fee: {} microAlgos".format(params.min_fee) ) 
-	closetoamt = account_info.get('amount') - (params.min_fee + amount)
-	print("Close to Amount: {} microAlgos".format(closetoamt) + "\n")
 
-	account_info = algod_client.account_info(my_address)
-	print("Final Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
+    print("Starting Account balance: {} microAlgos".format(
+        account_info.get('amount')))
+    print("Amount transfered: {} microAlgos".format(amount))
+
+
 
 create_account()
