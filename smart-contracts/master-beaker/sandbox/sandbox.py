9002b92bc4f9e4b3f45f24dae0472d599592f684
Chris Kim <chris.kim@algorand.com>
2023-04-09 22:55:28 +0900

    sandbox for master beaker added

---
+++ smart-contracts/master-beaker/sandbox/sandbox.py
@@ -0,0 +1,23 @@
+import json
+
+from beaker import sandbox
+
+algod_client = sandbox.get_algod_client()
+
+sp = algod_client.suggested_params()
+print("suggested minimum fee is: ", sp.min_fee, "\n")
+
+sandbox_accounts = sandbox.get_accounts()
+print(*sandbox_accounts, sep="\n\n")
+
+account1 = sandbox_accounts.pop()
+print("\naccount1 info \n")
+print("account1 address: ", account1.address, "\n")
+print("account1 private key: ", account1.private_key, "\n")
+print("account1 signer: ", account1.signer, "\n")
+
+indexer_client = sandbox.get_indexer_client()
+
+block_info = indexer_client.block_info(1)
+print("Block 1 information using the indexer client: \n")
+print(json.dumps(block_info, indent=2))
