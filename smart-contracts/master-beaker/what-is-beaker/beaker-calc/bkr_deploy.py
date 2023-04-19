901a60e781f5b95ba36b4031c449e49dcf906eee
Chris Kim <chris.kim@algorand.com>
2023-04-19 14:23:34 -0700

    file name changed

---
+++ smart-contracts/master-beaker/what-is-beaker/beaker-calc/bkr_deploy.py
@@ -0,0 +1,26 @@
+from bkr_contract import app, add
+from beaker import sandbox, client
+
+app.build().export("./artifacts")
+
+accounts = sandbox.kmd.get_accounts()
+sender = accounts[0]
+
+app_client = client.ApplicationClient(
+    client=sandbox.get_algod_client(),
+    app=app,
+    sender=sender.address,
+    signer=sender.signer,
+)
+
+app_id, addr, txid = app_client.create()
+print(
+    f"""
+Deployed app in txid {txid}
+App ID: {app_id}
+App Address: {addr}
+"""
+)
+
+return_value = app_client.call(add, a=10, b=5).return_value
+print(f"add => {return_value}")
